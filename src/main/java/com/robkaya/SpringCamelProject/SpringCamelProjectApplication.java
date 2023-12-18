19cdfc1203b034b87b09d7b15eff262da3e83e1d
RobKaya <robkaya@icloud.com>
2023-12-18 10:58:39 +0100

    Original commit

---
+++ src/main/java/com/robkaya/SpringCamelProject/SpringCamelProjectApplication.java
@@ -0,0 +1,13 @@
+package com.robkaya.SpringCamelProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCamelProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCamelProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/robkaya/SpringCamelProject/SpringCamelProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robkaya.SpringCamelProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCamelProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
