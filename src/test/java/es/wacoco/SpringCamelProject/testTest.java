9f05312bac46cc1502a5e5e0c0dddbc31ee82bbc
stykle <Maxamadabdulahist@outlook.com>
2024-01-11 05:51:39 +0100

    commit

---
+++ src/test/java/es/wacoco/SpringCamelProject/testTest.java
@@ -0,0 +1,15 @@
+package es.wacoco.SpringCamelProject;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class testTest {
+
+    @Test
+    void hello(){
+
+        String hello = "hello";
+        assertEquals(hello, "hello");
+    }
+}
