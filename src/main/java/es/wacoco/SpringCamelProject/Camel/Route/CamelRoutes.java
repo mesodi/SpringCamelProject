a2e255fd78c416c771ef9001c49d2e941b68eb8a
stykle <Maxamadabdulahist@outlook.com>
2024-01-12 12:04:11 +0100

    commit

--- src/main/java/es/wacoco/SpringCamelProject/Camel/Route/CamelRoutes.java
+++ src/main/java/es/wacoco/SpringCamelProject/Camel/Route/CamelRoutes.java
@@ -7,6 +7,9 @@ import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Map;
+
 @Component
 public class CamelRoutes extends RouteBuilder {
     @Override
